3803a1c5fa76c9cd0a4e27bd49ba9d44aed0bba9
EdoLelli <edoardo_lelli@libero.it>
2013-10-18 13:22:29 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chirp::Application.config.secret_key_base = '1c60d10c93fe39d4df0fe075f7a6d576e6f732bf3508f1b3e453b8fec7e18245a6a9c625f2df7652a954a6f3308b609c609068a654865ad6a7f984e98a59efbb'
